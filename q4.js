6e812766dfbead76873bbc7388a4086d0c618981
Natural0rder <102281652+Natural0rder@users.noreply.github.com>
2022-08-15 23:54:27 +0200

    Update q4.js

--- q4.js
+++ q4.js
@@ -1,19 +1,19 @@
 db.hotelSearch.aggregate([
-    {
-      $search: {
-        "autocomplete": {
-          "path": "hotelName",
-          "query": "[word]"
-        }
-      }
-    },
-    {
-      $limit: 10
-    },
-    {
-      $project: {
-        "_id": 0,
-        "hotelName": 1
+  {
+    $search: {
+      "autocomplete": {
+        "path": "hotelName",
+        "query": "[word]"
       }
     }
-  ])
+  },
+  {
+    $limit: 10
+  },
+  {
+    $project: {
+      "_id": 0,
+      "hotelName": 1
+    }
+  }
+])
