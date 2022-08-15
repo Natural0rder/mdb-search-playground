b48616b6f942705f66a9d14ec6139b2fe780365a
Natural0rder <102281652+Natural0rder@users.noreply.github.com>
2022-08-15 23:53:25 +0200

    Update q1.js

--- q1.js
+++ q1.js
@@ -1,27 +1,60 @@
 db.hotelSearch.aggregate([
-    {
-      $geoNear: {
-        near: {
-          type: "Point",
-          coordinates: [
-            12.496366,
-            41.902782
-          ]
-        },
-        distanceField: "dist.calculated",
-        maxDistance: 1000,
-        spherical: true
-      }
-    },
-    {
-      $limit: 10
-    },
-    {
-      $project: {
-        _id: 0,
-        hotelName: 1,
-        propertyId: 1,
-        "dist.calculated": 1
+  {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [
+          12.496366,
+          41.902782
+        ]
+      },
+      distanceField: "dist.calculated",
+      maxDistance: 10000,
+      spherical: true
+    }
+  },
+  {
+    $limit: 100
+  },
+  {
+    $project: {
+      _id: 0,
+      hotelName: 1,
+      propertyId: 1,
+      "dist.calculated": 1
+    }
+  }
+])
+
+db.hotelSearch.aggregate([
+  {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [
+          12.496366,
+          41.902782
+        ]
+      },
+      distanceField: "dist.calculated",
+      maxDistance: 10000,
+      spherical: true
+    }
+  },
+  {
+    $group: {
+      _id: {
+        name: "$hotelName",
+        distance: "$dist.calculated"
+      },
+      desc: {
+        $push: {
+          propertyId: "$propertyId"
+        }
       }
     }
-  ])
+  },
+  {
+    $limit: 100
+  }
+])
