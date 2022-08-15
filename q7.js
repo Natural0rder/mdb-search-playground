fdeeac9d947932d1bb7e48e34b0368f35b958517
Natural0rder <102281652+Natural0rder@users.noreply.github.com>
2022-08-15 23:55:42 +0200

    Update q7.js

--- q7.js
+++ q7.js
@@ -1,45 +1,45 @@
 db.hotelSearch.aggregate([
-    {
-      $search: {
-        "compound": {
-          "must": [
-            {
-              "autocomplete": {
-                "query": "[word]",
-                "path": "hotelName",
-                "fuzzy": {
-                  "maxEdits": 2,
-                  "prefixLength": 3
-                }
-              }
-            },
-            {
-              "geoWithin": {
-                "circle": {
-                  "center": {
-                    "type": "Point",
-                    "coordinates": [
-                      12.496366,
-                      41.902782
-                    ]
-                  },
-                  "radius": 1000
-                },
-                "path": "geoCode"
+  {
+    $search: {
+      "compound": {
+        "must": [
+          {
+            "autocomplete": {
+              "query": "[word]",
+              "path": "hotelName",
+              "fuzzy": {
+                "maxEdits": 2,
+                "prefixLength": 3
               }
             }
-          ]
-        }
-      }
-    },
-    {
-      $project: {
-        "_id": 0,
-        "propertyId": 1,
-        "hotelName": 1,
-        score: {
-          $meta: "searchScore"
-        }
+          },
+          {
+            "geoWithin": {
+              "circle": {
+                "center": {
+                  "type": "Point",
+                  "coordinates": [
+                    12.496366,
+                    41.902782
+                  ]
+                },
+                "radius": 10000
+              },
+              "path": "geoCode"
+            }
+          }
+        ]
       }
     }
-  ])
+  },
+  {
+    $project: {
+      "_id": 0,
+      "propertyId": 1,
+      "hotelName": 1,
+      score: {
+        $meta: "searchScore"
+      }
+    }
+  }
+])
