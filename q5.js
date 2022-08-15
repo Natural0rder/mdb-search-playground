442d6955d670b02c2c121fd297791df5e4e956d5
Natural0rder <102281652+Natural0rder@users.noreply.github.com>
2022-08-15 23:55:02 +0200

    Update q5.js

--- q5.js
+++ q5.js
@@ -1,32 +1,32 @@
 db.hotelSearch.aggregate([
-    {
-      $search: {
-        "geoWithin": {
-          "circle": {
-            "center": {
-              "type": "Point",
-              "coordinates": [
-                12.496366,
-                41.902782
-              ]
-            },
-            "radius": 1000
+  {
+    $search: {
+      "geoWithin": {
+        "circle": {
+          "center": {
+            "type": "Point",
+            "coordinates": [
+              12.496366,
+              41.902782
+            ]
           },
-          "path": "geoCode"
-        }
-      }
-    },
-    {
-      $limit: 10
-    },
-    {
-      $project: {
-        "_id": 0,
-        "propertyId": 1,
-        "hotelName": 1,
-        score: {
-          $meta: "searchScore"
-        }
+          "radius": 10000
+        },
+        "path": "geoCode"
       }
     }
-  ])
+  },
+  {
+    $limit: 100
+  },
+  {
+    $project: {
+      "_id": 0,
+      "propertyId": 1,
+      "hotelName": 1,
+      score: {
+        $meta: "searchScore"
+      }
+    }
+  }
+])
