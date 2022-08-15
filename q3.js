3d290dd036171b0fa2ef9343b031ab0d9543fc9b
Natural0rder <102281652+Natural0rder@users.noreply.github.com>
2022-08-15 23:54:09 +0200

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,59 +1,88 @@
 db.hotelSearch.aggregate([
-    {
-      $geoNear: {
-        near: {
-          type: "Point",
-          coordinates: [
-            12.496366,
-            41.902782
-          ]
-        },
-        distanceField: "dist.calculated",
-        maxDistance: 1000,
-        spherical: true
-      }
-    },
-    {
-      $match: {
-        amenityCodes: {
-          $all: [
-            "RMA.95",
-            "RMA.13"
-          ]
-        }
-      }
-    },
-    {
-      $limit: 100
-    },
-    {
-      $group: {
-        _id: {
-          "physicalHotel": "$dupeId",
-          "distance": "$dist.calculated",
-          "name": "$hotelName",
-          "amenities": "$amenityCodes"
-        },
-        offersCount: {
-          $sum: 1
-        },
-        hotels: {
-          $push: {
-            provider: {
-              $concat: [
-                "$chainCode",
-                "-",
-                "$iataCode"
-              ]
-            },
-            property: "$propertyId"
-          }
-        }
-      }
-    },
-    {
-      $sort: {
-        "_id.distance": 1
+  {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [
+          12.496366,
+          41.902782
+        ]
+      },
+      distanceField: "dist.calculated",
+      maxDistance: 10000,
+      spherical: true
+    }
+  },
+  {
+    $match: {
+      "address.countryCode": "IT",
+      "address.postalCode": {
+        $in: [
+          "00136",
+          "00141",
+          "00198",
+          "00146",
+          "00147",
+          "00153",
+          "00155",
+          "00156",
+          "00158",
+          "00159",
+          "00161",
+          "00164",
+          "00165",
+          "00172",
+          "00176",
+          "00182",
+          "00183",
+          "00184",
+          "00185",
+          "00186",
+          "00187",
+          "00192",
+          "00193",
+          "00197"
+        ]
+      },
+      amenityCodes: {
+        $all: [
+          "RMA.95",
+          "RMA.13"
+        ]
       }
     }
-  ])
+  },
+  {
+    $limit: 100
+  },
+  {
+    $group: {
+      _id: {
+        "physicalHotel": "$dupeId",
+        "distance": "$dist.calculated",
+        "name": "$hotelName",
+        "amenities": "$amenityCodes"
+      },
+      offersCount: {
+        $sum: 1
+      },
+      hotels: {
+        $push: {
+          provider: {
+            $concat: [
+              "$chainCode",
+              "-",
+              "$iataCode"
+            ]
+          },
+          property: "$propertyId"
+        }
+      }
+    }
+  },
+  {
+    $sort: {
+      "_id.distance": 1
+    }
+  }
+])
